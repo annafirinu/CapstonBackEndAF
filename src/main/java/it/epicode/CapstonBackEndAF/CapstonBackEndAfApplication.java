0d4e31236492587f9d24a50c55ab504cadab0473
annafirinu <anna.firinu2@outlook.it>
2025-07-01 11:45:18 +0200

    Initial commit

---
+++ src/main/java/it/epicode/CapstonBackEndAF/CapstonBackEndAfApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.CapstonBackEndAF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonBackEndAfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstonBackEndAfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/CapstonBackEndAF/CapstonBackEndAfApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.CapstonBackEndAF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstonBackEndAfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
